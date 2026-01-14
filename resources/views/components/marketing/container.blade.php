4daa1da5d6c7cccd827bb9459b49b5b3eef8b105
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2026-01-14 10:55:07 -0600

    ui: make marketing page more compact and consistent with zinc color scheme for better mobile experience

--- resources/views/components/marketing/container.blade.php
+++ resources/views/components/marketing/container.blade.php
@@ -1,5 +1,5 @@
-<div {{ $attributes->class('px-6 lg:px-8') }}>
-    <div class="mx-auto max-w-2xl lg:max-w-7xl">
+<div {{ $attributes->class('px-6') }}>
+    <div class="mx-auto max-w-2xl">
         {{ $slot }}
     </div>
 </div>
