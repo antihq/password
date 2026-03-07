d5a1388b18d00bc5b0dc41cf729b9a55f598fa89
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2026-03-06 19:22:02 -0600

    wip

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,5 +23,11 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::shouldBeStrict();
+
+        Gate::define('viewPulse', function (User $user): bool {
+            $adminEmail = config('admin.email');
+
+            return $adminEmail && $user->email === $adminEmail;
+        });
     }
 }
---
+++ config/admin.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Admin Email
+    |--------------------------------------------------------------------------
+    |
+    | This value specifies the email address of the user who has admin access
+    | to various admin-only features like Laravel pulse, etc. This should
+    | be set in your .env file for security.
+    |
+    */
+
+    'email' => env('ADMIN_EMAIL'),
+
+];
